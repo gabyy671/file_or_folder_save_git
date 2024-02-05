309e4b9554ca24fbb4f2561d027073f6ea70ad4a
gabyy671 <gabylivelol123@gmail.com>
2024-02-05 11:02:03 +0100

    dossier

--- script.bat
+++ script.bat
@@ -1,12 +1,15 @@
 @echo off
 
-REM Chemin du fichier source (dossier)
-set "dossier_source=C:\Users\maxime\Documents\test\heeey"
+REM Chemin du dossier ou du fichier source
+set "source=C:\Users\maxime\Documents\test\heeey"
 
-REM Vérifier si le chemin spécifié est un dossier
-if not exist "%dossier_source%\*" (
-    echo Le chemin spécifié n'est pas un dossier valide.
-    exit /B 1
+REM Vérifier si le chemin spécifié est un dossier ou un fichier
+if exist "%source%\*" (
+    REM C'est un dossier
+    set "est_dossier=true"
+) else (
+    REM C'est un fichier
+    set "est_dossier=false"
 )
 
 REM Dossier de destination (répertoire actuel)
@@ -15,13 +18,17 @@ set "dossier_destination=%CD%"
 REM Nom du fichier journal (log)
 set "fichier_log=%dossier_destination%\git_log.txt"
 
-REM Copier le dossier et son contenu dans le dossier spécifié
-xcopy /Y /E "%dossier_source%" "%dossier_destination%"
+REM Copier le dossier ou le fichier dans le dossier spécifié
+if %est_dossier%==true (
+    xcopy /Y /E /I "%source%" "%dossier_destination%"
+) else (
+    copy /Y "%source%" "%dossier_destination%"
+)
 
 REM Aller dans le dossier de destination
 cd /D "%dossier_destination%" || exit /B 1
 
-REM Ajouter le dossier et son contenu au suivi de Git
+REM Ajouter le dossier ou le fichier au suivi de Git
 git add .
 
 REM Demander le message de commit à l'utilisateur
@@ -31,4 +38,4 @@ REM Effectuer un commit avec le message fourni
 git commit -m "%commit_message%" > "%fichier_log%" 2>&1
 
 REM Pousser les modifications vers le référentiel distant
-git push origin main >> "%fichier_log%" 2>&1
+::git push origin main >> "%fichier_log%" 2>&1
