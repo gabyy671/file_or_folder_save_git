cdde7300b24b9b8a80c739482a2b0285a7510df0
gabyy671 <gabylivelol123@gmail.com>
2024-02-05 10:57:27 +0100

    test dosssier 2

--- script.bat
+++ script.bat
@@ -1,21 +1,32 @@
 @echo off
 
 REM Chemin du fichier source
-::set "fichier_source=C:\Users\maxime\Documents\test\hellow ord.txt"
 set "fichier_source=C:\Users\maxime\Documents\test\heeey"
+
+REM Vérifier si le chemin spécifié est un fichier ou un dossier
+if exist "%fichier_source%\*" (
+    set "est_dossier=true"
+) else (
+    set "est_dossier=false"
+)
+
 REM Dossier de destination (répertoire actuel)
 set "dossier_destination=%CD%"
 
 REM Nom du fichier journal (log)
 set "fichier_log=%dossier_destination%\git_log.txt"
 
-REM Copier le fichier dans le dossier spécifié (en écrasant s'il existe déjà)
-copy /Y "%fichier_source%" "%dossier_destination%"
+REM Copier le fichier ou le dossier dans le dossier spécifié (en écrasant s'il existe déjà)
+if %est_dossier%==true (
+    xcopy /Y /E "%fichier_source%" "%dossier_destination%"
+) else (
+    copy /Y "%fichier_source%" "%dossier_destination%"
+)
 
 REM Aller dans le dossier de destination
 cd /D "%dossier_destination%" || exit /B 1
 
-REM Ajouter le fichier au suivi de Git
+REM Ajouter le fichier ou le dossier au suivi de Git
 git add .
 
 REM Demander le message de commit à l'utilisateur
