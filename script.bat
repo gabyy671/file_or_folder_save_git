96a21dfc975e4287083d5da713021b7f96697605
gabyy671 <gabylivelol123@gmail.com>
2024-02-05 11:53:30 +0100

    can now copy a folder or a file

--- script.bat
+++ script.bat
@@ -1,12 +1,14 @@
 @echo off
 
 REM Chemin du dossier ou du fichier source
-set "source=C:\Users\maxime\Documents\test\heeey"
-
+set "source=C:\Users\maxime\Documents\test\heeey\"
+mkdir world
 REM Vérifier si le chemin spécifié est un dossier ou un fichier
 if exist "%source%\*" (
     REM C'est un dossier
     set "est_dossier=true"
+
+
 ) else (
     REM C'est un fichier
     set "est_dossier=false"
@@ -20,13 +22,12 @@ set "fichier_log=%dossier_destination%\git_log.txt"
 
 REM Copier le dossier ou le fichier dans le dossier spécifié
 if %est_dossier%==true (
-    xcopy /Y /E /I "%source%" "%dossier_destination%"
-) else (
-    copy /Y "%source%" "%dossier_destination%"
-)
+    xcopy /Y /E "%source%" "%dossier_destination%\world"
 
-REM Aller dans le dossier de destination
-cd /D "%dossier_destination%" || exit /B 1
+
+) else (
+    copy /Y "%source%" "%dossier_destination%\world"
+)
 
 REM Ajouter le dossier ou le fichier au suivi de Git
 git add .
@@ -38,4 +39,5 @@ REM Effectuer un commit avec le message fourni
 git commit -m "%commit_message%" > "%fichier_log%" 2>&1
 
 REM Pousser les modifications vers le référentiel distant
-::git push origin main >> "%fichier_log%" 2>&1
+git push origin main >> "%fichier_log%" 2>&1
+pause
---
+++ test.bat
@@ -0,0 +1,3 @@
+mkdir dossier1
+mkdir dossier2
+xcopy /Y /E /I "dossier1" "dossier2\dossier1"
